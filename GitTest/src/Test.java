91f7b4d3c15a028ef1d23a6a6b98de29115e7bcf
192190 <192190@p333b11.ii.local>
2014-10-14 08:38:39 +0200

    kk

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,9 +2,13 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.print("test");
-
 		//hsiohgvisdhgohshgoshd
+		//Ewelina comment vlhbhih
 		//Ewelina comment jnjnjkn
 		//feef
+		
+		
+		//Ewelina comment vlhbhih
+		//Ewelina comment vlhbhih
 	}
 }
