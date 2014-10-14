2ce52b66fe429d9651e5b5501be6a48c179fd522
192190 <192190@p333b11.ii.local>
2014-10-14 08:41:38 +0200

    okok

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -3,12 +3,8 @@ public class Test {
 	public static void main(String[] args){
 		System.out.print("test");
 		//hsiohgvisdhgohshgoshd
-		//Ewelina comment vlhbhih
 		//Ewelina comment jnjnjkn
-		//feef
-		
-		
-		//Ewelina comment vlhbhih
+		//fee
 		//Ewelina comment vlhbhih
 	}
 }
