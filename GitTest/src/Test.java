cbe28dca640eaba6e64c8562c44934ce5ae23945
192190 <192190@p333b11.ii.local>
2014-10-14 08:27:13 +0200

    lklk

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,7 +2,9 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.print("test");
+
 		//hsiohgvisdhgohshgoshd
-		//Ewelina comment
+		//Ewelina comment jnjnjkn
+		//feef
 	}
 }
